0741e52cc96f34e664625709007194dfa4364458
Jordan Jesu <110118742+JordanRino@users.noreply.github.com>
2026-04-06 19:18:54 +0000

    (chore) update setup

--- get_password_hash.py
+++ get_password_hash.py
@@ -1,8 +1,11 @@
 import bcrypt
 
 def hash_password() -> str:
-    password = admin
+    password = "admin"
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
     print(hashed.decode("utf-8"))
     return hashed.decode("utf-8")
+
+if __name__ == "__main__":
+    hash_password()
